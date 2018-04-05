d672e0a1a21f8bb7b641261567cd01fcd1b0d1bd
Ivan Stroykin <s.t.i.vin@mail.ru>
2018-04-05 14:33:08 +0300

    fix

--- src/app/shared/shared-layout/toolbar/toolbar.component.ts
+++ src/app/shared/shared-layout/toolbar/toolbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+const GIT_HUB_LINK: string = 'https://github.com/Stivin/angular-universal';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html'
 })
 export class ToolbarComponent {
+  public gitHubLint: string = GIT_HUB_LINK;
 }
