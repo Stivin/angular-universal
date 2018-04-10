a0e625106934af7d625b456007d0c832261976ba
Ivan Stroykin <s.t.i.vin@mail.ru>
2018-04-10 19:37:03 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,16 @@
-// Work around for https://github.com/angular/angular-cli/issues/7200
-
-const path = require('path');
-const webpack = require('webpack');
+const PATH = require('path');
+const WEBPACK = require('webpack');
 
 module.exports = {
   entry: {
-    // This is our Express server for Dynamic universal
     server: './server.ts',
-    // This is an example of Static prerendering (generative)
     prerender: './prerender.ts'
   },
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
-  // Make sure we include all node_modules etc
-  externals: [/(node_modules|main\..*\.js)/,],
+  externals: [/(node_modules|main\..*\.js)/],
   output: {
-    // Puts the output at the root of the dist folder
-    path: path.join(__dirname, 'dist'),
+    path: PATH.join(__dirname, 'dist'),
     filename: '[name].js'
   },
   module: {
@@ -25,17 +19,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
-      /(.+)?angular(\\|\/)core(.+)?/,
-      path.join(__dirname, 'src'), // location of your src
-      {} // a map of your routes
-    ),
-    new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
-      /(.+)?express(\\|\/)(.+)?/,
-      path.join(__dirname, 'src'),
-      {}
-    )
+    new WEBPACK.ContextReplacementPlugin(/(.+)?angular(\\|\/)core(.+)?/, PATH.join(__dirname, 'src'), {}),
+    new WEBPACK.ContextReplacementPlugin(/(.+)?express(\\|\/)(.+)?/, PATH.join(__dirname, 'src'), {})
   ]
-}
+};
