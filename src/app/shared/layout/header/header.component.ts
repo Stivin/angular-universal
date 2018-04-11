1036542eef3aa04f0795a9272db13dee695a9d44
Ivan Stroykin <s.t.i.vin@mail.ru>
2018-04-11 16:58:38 +0300

    fix

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Input() title: string;
+  @Input() description: string;
 }
