3058d1d6a4f4c68d85fd4e2f8ccc5a044c688f49
Ivan Stroykin <s.t.i.vin@mail.ru>
2018-04-18 18:30:58 +0300

    fix position footer

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,16 +1,17 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component } from '@angular/core';
 
 const ANGULAR_IMG: string = 'assets/img/angular-white-transparent.svg';
 const ANGULAR_LINK: string = 'https://angular.io';
 const VERSION: string = '5.2.9';
+const YEAR: number = 2018;
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
-  @HostBinding('class.footer-wrap') classFooterWrap: boolean = true;
   public angularImg: string = ANGULAR_IMG;
   public angularLink: string = ANGULAR_LINK;
   public version: string = VERSION;
+  public year: number = YEAR;
 }
