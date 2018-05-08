102367413cf84435d61e9821cfa16799f37f9f47
Ivan Stroykin <s.t.i.vin@mail.ru>
2018-05-08 17:43:15 +0300

    update manifest json and fix toolbar

--- src/app/shared/layout/toolbar/toolbar-logo/toolbar-logo.component.ts
+++ src/app/shared/layout/toolbar/toolbar-logo/toolbar-logo.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+const ANGULAR_LOGO: string = 'assets/img/angular-white-transparent.svg';
+
 @Component({
   selector: 'app-toolbar-logo',
   templateUrl: './toolbar-logo.component.html'
 })
 export class ToolbarLogoComponent {
+  public angularImg: string = ANGULAR_LOGO;
 }
